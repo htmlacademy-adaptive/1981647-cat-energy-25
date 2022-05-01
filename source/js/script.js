27d1321a3b0eefd9fad81af7eb4bed1a636b060d
Polina Bartles <polina.bartles@gmail.com>
2022-05-01 11:08:20 -0500

    Module5-task1

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,8 @@
 let navMain = document.querySelector('.main-nav');
+let mainHeader = document.querySelector('.main-header');
 let navToggle = document.querySelector('.main-nav__toggle');
 
-// navMain.classList.remove('main-nav--nojs');
+mainHeader.classList.remove('main-header--no-js');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
