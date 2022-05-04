02fa4b13ae762ecd87d06f840c2f6b82cca7b4ca
Polina Bartles <polina.bartles@gmail.com>
2022-05-04 09:41:12 -0500

    Декоративные элементы внутренних страниц

--- source/templates/_data.js
+++ source/templates/_data.js
@@ -13,4 +13,19 @@ export default {
       "name": "Подбор программы",
     },
   ],
+  "viewports": {
+    "desktop": 1280,
+    "tablet": 768,
+    "mobile": null
+  },
+  "images": {
+    "products/chicken-small": {
+      "ext": "png",
+      "width": 68,
+      "height": 86,
+      "maxdppx": 2,
+      "alt": "Cat Energy Pro chicken.",
+      "viewports": ["desktop", "tablet", "mobile"]
+    }
+  }
 }
