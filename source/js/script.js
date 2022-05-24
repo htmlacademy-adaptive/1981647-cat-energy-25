4ecb5ceb011e9a0e68bac619be69f391551daabd
Polina Bartles <polina.bartles@gmail.com>
2022-05-23 23:29:40 -0500

    Исправляет хедер без js

--- source/js/script.js
+++ source/js/script.js
@@ -1,22 +1,22 @@
-let navMain = document.querySelector('.main-nav');
-let mainHeader = document.querySelector('.main-header');
-let navToggle = document.querySelector('.main-nav__toggle');
+// let navMain = document.querySelector('.main-nav');
+// let mainHeader = document.querySelector('.main-header');
+// let navToggle = document.querySelector('.main-nav__toggle');
 
-mainHeader.classList.remove('main-header--no-js');
+// mainHeader.classList.remove('main-header--no-js');
 
-navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-  }
-});
+// navToggle.addEventListener('click', function () {
+//   if (navMain.classList.contains('main-nav--closed')) {
+//     navMain.classList.remove('main-nav--closed');
+//     navMain.classList.add('main-nav--opened');
+//   } else {
+//     navMain.classList.add('main-nav--closed');
+//     navMain.classList.remove('main-nav--opened');
+//   }
+// });
 
-let contactMapIframe = document.querySelector('.contacts__map--iframe');
-let contactMapImage = document.querySelector('.contacts__map--image');
+// let contactMapIframe = document.querySelector('.contacts__map--iframe');
+// let contactMapImage = document.querySelector('.contacts__map--image');
 
 
-contactMapIframe.style.display = "block";
-contactMapImage.style.display = "none";
+// contactMapIframe.style.display = "block";
+// contactMapImage.style.display = "none";
